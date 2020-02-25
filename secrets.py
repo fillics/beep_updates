09647776343a1aa210c62201ea5dab816ecfc71f
Filippo Caliò <filippo.calio99@gmail.com>
2020-02-25 12:04:38 +0100

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,9 @@
+codice_utente = your_codice_utente
+password_utente = 'your_password'
+
+# Mittente
+email_user = 'email_dalla_quale_ricevere_comunicazioni'
+email_pass = 'password_dell_email'
+
+# Destinatario
+email_send = 'email_sulla_quale_ricevere_comunicazioni'
