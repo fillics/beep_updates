5800732265e2be176b52f9e880227c5d20ff2b13
Walter Carli <walkersneps@gmail.com>
2020-11-15 18:16:40 +0100

    Segmentazione in funzioni
    (purtroppo non ancora ordinatissime ^^)

--- beep_updates.py
+++ beep_updates.py
@@ -1,7 +1,95 @@
+def send_email_notif(link, corso):
+	print("Invio email...")
+	subject = 'Nuovo file caricato su '+corso
+	msg = MIMEText("Ciao, e' appena stato caricato su Beep un file. Il link per scaricarlo e' il seguente: ", link)
+	msg['From'] = email_user
+	msg['To'] = email_send
+	msg['subject'] = subject
+	server = smtplib.SMTP('smtp.gmail.com:587')
+	server.starttls()
+	server.ehlo()
+	server.login(email_user, email_pass)
+	server.sendmail(email_user, email_send, msg.as_string())
+	server.quit()
+
+def send_webpush_notif(link, listaDocumenti):
+	print("Invio notifica push...")
+	notify.send('Nuovo file caricato su Beep: '+str(listaDocumenti[0]), link)
+
+def send_notifs(link, corso, listaDocumenti):
+	if notif_push == 'true': send_webpush_notif(link, listaDocumenti)
+	if notif_email == 'true': send_email_notif(link, corso)
+
+def init_browser(): # -> browser
+	print("Let's go!\n")
+	l.info("\nOpening browser...")
+	if headless == 'true' and pi_mode != 'true':
+		browser = webdriver.Chrome(executable_path=chromedriver_bin, options=chrome_options)
+	else:
+		browser = webdriver.Firefox() if pi_mode == 'true' else Chrome()
+
+	return browser
+
+def connect_to_beep(browser):
+	url = 'https://beep.metid.polimi.it/'
+	l.info("\nNavigating...")
+	browser.get(url) # connessione a beep
+	if pi_mode == 'true': browser.minimize_window() # iconizzare il browser
+	sleep(2)
+
+def do_beep_login(browser):
+	# Click del pulsante Login
+	button = browser.find_element_by_xpath("//a[contains(text(), 'Login')]")
+	button.click()
+	sleep(2)
+
+	# Riempimento con le credenziali
+	print("\nEseguo il login...")
+	login_field = browser.find_element_by_xpath("//input[@name=\"login\"]").send_keys(codice_utente)
+	pw_field = browser.find_element_by_xpath("//input[@name=\"password\"]").send_keys(password_utente)
+	sleep(2)
+
+	# Click del pulsante di Login
+	button = browser.find_element_by_xpath("//button[@name=\"evn_conferma\"]")
+	button.click()
+	sleep(3)
+
+def cerca_corsi(browser): # -> corsi_disponibili
+	print("\nI tuoi corsi disponibili sono i seguenti:")
+	corsi_disponibili = []
+	#corsi = browser.find_elements_by_xpath("//*[contains(text(), anno_accademico)]")
+	corsi = browser.find_elements_by_xpath("//*[contains(text(), '[2020-21]')]")
+	for x in corsi:
+		corsi_disponibili.append(x.text)
+
+	corsi_disponibili = list(dict.fromkeys(corsi_disponibili))
+	print('\n'.join(corsi_disponibili))
+	return corsi_disponibili
+
+def get_corsi_desiderati(quanti_corsi): # -> corsi_desiderati
+	print("\nHai specificato di voler seguire " + str(quanti_corsi) + " corsi.\nLi cercherò come specificato nelle impostazioni.\n")
+	corsi_desiderati = []
+	# costruisco la lista dei nomi dei corsi
+	if headless == 'true': # se siamo headless, cercali nelle variabili d'ambiente
+		for i in range(1, quanti_corsi+1):
+			corsi_desiderati.append(os.environ.get('CORSO_SCELTO_'+str(i)))
+	else: # altrimenti, prendili dal file di Configurazione
+		corsi_desiderati = [corso_scelto_1, corso_scelto_2, corso_scelto_3, corso_scelto_4, corso_scelto_5, corso_scelto_6, corso_scelto_7]
+
+	return corsi_desiderati
+
+
+
+
+
+
+
+
+
+
 print("-o-o-o-o- Beep Updates -o-o-o-o-o-")
 
 
-## _-_-_-_ IMPORTS _-_-_-_
 import logging as l
 l.info("\nImporting...")
 from time import sleep
@@ -37,7 +125,7 @@ if notif_email == 'true':
 	from email import encoders
 l.info("Done importing!\n")
 
-## _-_-_-_ BROWSER SETUP and ENV VARS _-_-_-_
+## _-_-_-_  ENV VARS _-_-_-_
 if headless == 'true' and pi_mode != 'true':
 	print("Headless mode detected!")
 
@@ -65,93 +153,18 @@ else:
 
 
 
-## _-_-_-_ FUNCTIONS DEFINITIONS _-_-_-_
-def send_email_notif(link, corso):
-	print("Invio email...")
-	subject = 'Nuovo file caricato su '+corso
-	msg = MIMEText("Ciao, e' appena stato caricato su Beep un file. Il link per scaricarlo e' il seguente: ", link)
-	msg['From'] = email_user
-	msg['To'] = email_send
-	msg['subject'] = subject
-	server = smtplib.SMTP('smtp.gmail.com:587')
-	server.starttls()
-	server.ehlo()
-	server.login(email_user, email_pass)
-	server.sendmail(email_user, email_send, msg.as_string())
-	server.quit()
-
-def send_webpush_notif(link, listaDocumenti):
-	print("Invio notifica push...")
-	notify.send('Nuovo file caricato su Beep: '+str(listaDocumenti[0]), link)
-
-def send_notifs(link, corso, listaDocumenti):
-	if notif_push == 'true': send_webpush_notif(link, listaDocumenti)
-	if notif_email == 'true': send_email_notif(link, corso)
-
-def connect_to_beep(browser):
-	url = 'https://beep.metid.polimi.it/'
-	l.info("\nNavigating...")
-	browser.get(url) # connessione a beep
-	if pi_mode == 'true': browser.minimize_window() # iconizzare il browser
-	sleep(2)
-
-def do_beep_login(browser):
-	# Click del pulsante Login
-	button = browser.find_element_by_xpath("//a[contains(text(), 'Login')]")
-	button.click()
-	sleep(2)
-
-	# Riempimento con le credenziali
-	print("\nEseguo il login...")
-	login_field = browser.find_element_by_xpath("//input[@name=\"login\"]").send_keys(codice_utente)
-	pw_field = browser.find_element_by_xpath("//input[@name=\"password\"]").send_keys(password_utente)
-	sleep(2)
-
-	# Click del pulsante di Login
-	button = browser.find_element_by_xpath("//button[@name=\"evn_conferma\"]")
-	button.click()
-	sleep(3)
-
-
-
-
-# Apertura Browser
-print("Let's go!\n")
-l.info("\nOpening browser...")
-if headless == 'true' and pi_mode != 'true':
-	browser = webdriver.Chrome(executable_path=chromedriver_bin, options=chrome_options)
-else:
-	browser = webdriver.Firefox() if pi_mode == 'true' else Chrome()
+# Inizializzazione browser
+browser = init_browser()
 
 # Connessione a BeeP e login
 connect_to_beep(browser)
 do_beep_login(browser)
 
-# Chiedere quale corso seguire
-corsi_disponibili = []
-#corsi = browser.find_elements_by_xpath("//*[contains(text(), anno_accademico)]")
-corsi = browser.find_elements_by_xpath("//*[contains(text(), '[2020-21]')]")
-for x in corsi:
-	corsi_disponibili.append(x.text)
+# Ottieni liste dei corsi
+corsi_disponibili = cerca_corsi(browser)
+corsi_desiderati = get_corsi_desiderati(quanti_corsi)
 
-corsi_disponibili = list(dict.fromkeys(corsi_disponibili))
-
-print("\nI tuoi corsi disponibili sono i seguenti:")
-sleep(0.5)
-print('\n'.join(corsi_disponibili))
-sleep(2)
-
-corsi_desiderati = []
-print("\nHai specificato di voler seguire " + str(quanti_corsi) + " corsi.\nLi cercherò come specificato nelle impostazioni.\n")
-
-# costruisco la lista dei nomi dei corsi
-if headless == 'true': # se siamo headless, cercali nelle variabili d'ambiente
-	for i in range(1, quanti_corsi+1):
-		corsi_desiderati.append(os.environ.get('CORSO_SCELTO_'+str(i)))
-else: # altrimenti, prendili dal file di Configurazione
-	corsi_desiderati = [corso_scelto_1, corso_scelto_2, corso_scelto_3, corso_scelto_4, corso_scelto_5, corso_scelto_6, corso_scelto_7]
-
-
+# Verifiche updates
 for i in range(0, quanti_corsi):
 	corso = corsi_desiderati[i].upper() # seleziono il corso
 
@@ -227,8 +240,7 @@ for i in range(0, quanti_corsi):
 	browser.back()
 	sleep(2)
 
+
 print("\n\nFinito! Buono Studio!\n\n\nSaluti al Magnifico Ferruccio!")
-
-
-sleep(3)
+sleep(2)
 browser.close()
