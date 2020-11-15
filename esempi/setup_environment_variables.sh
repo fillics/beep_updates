f0c6e1ef68f745382ebf6447dab790571406df9f
Walter Carli <walkersneps@gmail.com>
2020-11-15 17:22:02 +0100

    Aggiunto file di esempio per rapido setup di variabili d'ambiente

---
+++ esempi/setup_environment_variables.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export BEEP_CODICE_UTENTE="12345678"
+export BEEP_PASSWORD=password
+
+export HTTP_GET_ENDPOINT=http://httpbin.org/get
+export HTTP_POST_ENDPOINT=http://httpbin.org/post
+
+export CHROME_BIN='path/to/chrome/exec'
+export CHROMEDRIVER_PATH=/path/to/chromedriver
+
+export CORSO_SCELTO_1=analisi
+export CORSO_SCELTO_2=meccanica
+export CORSO_SCELTO_3=fisica
+export CORSO_SCELTO_4=tipregodammi18
